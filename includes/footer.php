c2343d8f96d17441830b60008e83eb42c464bbf8
Marion4000 <marion.marrot@gmail.com>
2022-07-20 13:53:11 +0200

    Save 1

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="contenu-interne">
+        <h2>Adresse</h2>
+        <p>Lorem ipsum dolor sit amet <br>
+    Muret</p>
+        <h2>Réseaux</h2>
+        <div>
+            <a href="facebook.fr"></a>
+            <a href="instagram.fr"></a>
+        </div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CINCO - Magasin de chaussures à Muret</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<?php include('includes/header.php');?>
+
+<main>
+    <!-- Banière -->
+    <section>
+        <div></div>
+    </section>
+
+    <!-- Galerie -->
+    <section>
+    <div class="contenu-interne">
+        <h2>Catalogue</h2>
+        <div>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+            <figure>
+                <img src="images/catalogue/paire1.jpg" alt="WWWWW">
+                <figcaption>WWWWWWWWWWW</figcaption>
+            </figure>
+        </div>
+    </div>    
+    </section>
+</main>
+
+<?php include('includes/footer.php');?>
+</body>
+</html>
